3f39f34913b04bf2cef1b375739c1d64951739c0
NerisD <dimitri@smith.re>
2021-10-26 11:10:43 +0300

    First Commit For My Project

---
+++ DimitriSMITH_Lab14_Protocol_PeaceOfCake/DimitriSMITH_Lab14_Protocol_PeaceOfCake/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DimitriSMITH_Lab14_Protocol_PeaceOfCake
+//
+//  Created by Dimitri SMITH on 26/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
