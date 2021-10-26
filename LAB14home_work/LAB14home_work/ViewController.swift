957e84818b9a81dfd2dfce853925c163e4b86355
Ehab hakami <ehab7k@gmail.com>
2021-10-26 14:18:17 +0300

    first commit for the project

---
+++ LAB14home_work/LAB14home_work/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LAB14home_work
+//
+//  Created by Ehab Hakami on 20/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
