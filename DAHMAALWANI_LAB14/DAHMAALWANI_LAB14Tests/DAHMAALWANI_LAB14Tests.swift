106971db8811f543cd4d3e969d500730aba5e691
DAHMAJABER <dahma.alwani.jiz.s@tuwaiq.edu.sa>
2021-10-27 21:10:31 +0300

    MY CAKE IS READY 🍎🥧

---
+++ DAHMAALWANI_LAB14/DAHMAALWANI_LAB14Tests/DAHMAALWANI_LAB14Tests.swift
@@ -0,0 +1,33 @@
+//
+//  DAHMAALWANI_LAB14Tests.swift
+//  DAHMAALWANI_LAB14Tests
+//
+//  Created by dahma alwani on 21/03/1443 AH.
+//
+
+import XCTest
+@testable import DAHMAALWANI_LAB14
+
+class DAHMAALWANI_LAB14Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
