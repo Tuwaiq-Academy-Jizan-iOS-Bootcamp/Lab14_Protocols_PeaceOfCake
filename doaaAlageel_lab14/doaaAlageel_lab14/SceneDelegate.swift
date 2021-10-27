c37cb43deae3edc7ee2050fa7d2c7e1d25e5447b
Doaa <doaa.ageel@gmail.com>
2021-10-27 10:01:49 +0300

    lab14

---
+++ doaaAlageel_lab14/doaaAlageel_lab14/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  doaaAlageel_lab14
+//
+//  Created by Dua'a ageel on 20/03/1443 AH.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
