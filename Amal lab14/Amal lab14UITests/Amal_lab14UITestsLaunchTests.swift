f170fe1dcf37478ee6a3305f9a5a346505dc1410
AMALJELI <Aml3mr@hotmail.com>
2021-10-27 09:59:20 +0300

    Amal  jeli lab 14 (MY Cake is ready 🧁😋)

---
+++ Amal lab14/Amal lab14UITests/Amal_lab14UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Amal_lab14UITestsLaunchTests.swift
+//  Amal lab14UITests
+//
+//  Created by Amal Jeli on 20/03/1443 AH.
+//
+
+import XCTest
+
+class Amal_lab14UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
