b2563ca62e60572d3c0c1f54a599fad31d13a5ab
Abdulrahman Gahzwni <Abdulrahman.gahzwni.jiz.s@tuwaiq.edu.sa>
2021-10-31 12:36:04 +0300

    uu

--- lab14/lab14/ViewController.swift
+++ lab14/lab14/ViewController.swift
@@ -43,13 +43,16 @@ protocol RespectTheCake {
      }
  }
 class ViewController: UIViewController {
-    var butter = DairyProduct.init(name: "Butter", number: 1, descreption: "Unsalted Butter")
-    var milk = DairyProduct.init(name: "Milk", number: 1, descreption: "Cow milk")
-    var apples = FruitProduct.init(name: "Apple", number: 2, descreption: "Peeled and sliced Apples")
-    var eggs = Otherproduct.init(name: "Egg", number: 2, descreption: "Chicken eggs")
-    var sugar = Otherproduct.init(name: "Sugar", number: 1, descreption: "Cup of Sugar")
+    
+    // init or not no problm
+    
+    var butter = DairyProduct(name: "Butter", number: 1, descreption: "Unsalted Butter")
+    var milk = DairyProduct(name: "Milk 🥛", number: 1, descreption: "Cow milk")
+    var apples = FruitProduct(name: "Apple 🍎", number: 2, descreption: "Peeled and sliced Apples")
+    var eggs = Otherproduct(name: "Egg 🥚", number: 2, descreption: "Chicken eggs")
+    var sugar = Otherproduct(name: "Sugar 🍚", number: 1, descreption: "Cup of Sugar")
     var flour = Otherproduct.init(name: "Flour", number: 2, descreption: "Cups of wheat flour")
-    var vanillia = Otherproduct.init(name: "Vanillia Extract", number: 1, descreption: "Tablespoone of Vanillia extract")
+    var vanillia = Otherproduct.init(name: "Vanillia Extract ", number: 1, descreption: "Tablespoone of Vanillia extract")
     override func viewDidLoad() {
         super.viewDidLoad()
         let bowl: [RespectTheCake] = [butter,milk,apples,eggs,sugar,flour,vanillia]
